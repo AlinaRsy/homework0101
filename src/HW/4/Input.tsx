3e8895e3027c6abb5d0465e6f92797ca2750b565
AlinaRsy <al.alinarsy616@gmail.com>
2026-05-12 23:26:02 +0300

    homework 0101

---
+++ src/HW/4/Input.tsx
@@ -0,0 +1,14 @@
+type InputPropsType = {
+	currentText: string; // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (value: string)=>void; // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Input = (props: InputPropsType) => {
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		// НУЖНО ДОПИСАТЬ
+		props.setCurrentText(event.target.value);
+	};
+	return (
+	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
+};
